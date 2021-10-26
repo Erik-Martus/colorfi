7f544ee204215803be5a4e1cc64afacea96f048d
Erik Martus <erik@emartus.dev>
2021-10-26 13:02:41 -0400

    :bento: Move icons into separate files
    Icons previously inline in components, moved to allow for possible future reusability

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import ghIcon from '../icons/github.svg';
 
 function Header() {
   return (
@@ -11,7 +12,7 @@ function Header() {
           target="_blank"
           rel="noreferrer"
         >
-          github
+          <img src={ghIcon} alt="GitHub" />
         </a>
       </nav>
     </header>
