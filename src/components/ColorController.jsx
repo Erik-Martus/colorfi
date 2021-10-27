a50063e1a1aee02747779b4b34f795896fd5a085
Erik Martus <erik@emartus.dev>
2021-10-27 16:43:18 -0400

    :sparkles: Add shade generation feature
    Generate shades based on input hex color, provided number of shades, base shade position, and hsl adjustments

--- src/components/ColorController.jsx
+++ src/components/ColorController.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { HexColorPicker, HexColorInput } from 'react-colorful';
 import Swatch from './Swatch';
+import calcShades from '../js/calcShades';
 import trashIcon from '../icons/trash.svg';
 
 function ColorController({ color, onThemeColorChange, onThemeColorRemove }) {
@@ -47,11 +48,29 @@ function ColorController({ color, onThemeColorChange, onThemeColorRemove }) {
   const [enableShade, setEnableShade] = useState(false);
   const handleShadeToggle = (event) => {
     setEnableShade(event.target.checked);
+    if (event.target.checked === true) {
+      setShades(
+        calcShades(
+          colorData.hex,
+          shadeAmount,
+          shadePosition,
+          hueAmount,
+          saturationAmount,
+          lightnessAmount
+        )
+      );
+    }
   };
 
   const [shadeAmount, setShadeAmount] = useState(9);
   const handleShadeAmountChange = (event) => {
     setShadeAmount(event.target.value);
+    setShadePosition(Math.round(event.target.value / 2));
+  };
+
+  const [shadePosition, setShadePosition] = useState(5);
+  const handleShadePositionChange = (event) => {
+    setShadePosition(event.target.value);
   };
 
   const [hueAmount, setHueAmount] = useState(0);
@@ -69,9 +88,32 @@ function ColorController({ color, onThemeColorChange, onThemeColorRemove }) {
     setLightnessAmount(event.target.value);
   };
 
+  const [shades, setShades] = useState([]);
+  useEffect(() => {
+    if (enableShade === true) {
+      setShades(
+        calcShades(
+          pickerColor,
+          shadeAmount,
+          shadePosition,
+          hueAmount,
+          saturationAmount,
+          lightnessAmount
+        )
+      );
+    }
+  }, [
+    pickerColor,
+    shadeAmount,
+    shadePosition,
+    hueAmount,
+    saturationAmount,
+    lightnessAmount,
+  ]);
+
   return (
     <div className="w-96">
-      <Swatch colors={[colorData]} />
+      <Swatch colors={enableShade ? shades : [colorData]} />
       <strong>{colorName ? colorName : 'Color Name'}</strong>
       <button role="button" onClick={handleRemove}>
         <img src={trashIcon} alt="Delete" />
@@ -84,7 +126,7 @@ function ColorController({ color, onThemeColorChange, onThemeColorRemove }) {
             type="text"
             value={colorName}
             onChange={handleColorNameChange}
-          ></input>
+          />
         </div>
         <div className="form-control">
           <HexColorPicker
@@ -119,15 +161,35 @@ function ColorController({ color, onThemeColorChange, onThemeColorRemove }) {
               type="range"
               value={shadeAmount}
               min="1"
-              max="10"
+              max="9"
               onChange={handleShadeAmountChange}
             />
             <input
               type="number"
               value={shadeAmount}
               min="1"
-              max="10"
+              max="9"
               onChange={handleShadeAmountChange}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor={`shadePos-${colorData.id}`}>
+              Base Color Position:
+            </label>
+            <input
+              id={`shadePos-${colorData.id}`}
+              type="range"
+              value={shadePosition}
+              min="1"
+              max={shadeAmount}
+              onChange={handleShadePositionChange}
+            />
+            <input
+              type="number"
+              value={shadePosition}
+              min="1"
+              max={shadeAmount}
+              onChange={handleShadePositionChange}
             />
           </div>
           <fieldset>
@@ -139,7 +201,7 @@ function ColorController({ color, onThemeColorChange, onThemeColorRemove }) {
                 type="number"
                 value={hueAmount}
                 min="0"
-                max="100"
+                max="360"
                 onChange={handleHueChange}
               />
             </div>
