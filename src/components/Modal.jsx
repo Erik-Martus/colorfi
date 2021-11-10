3158cbcd93577d7f54e75cb4b065eac5e0a7b00b
Erik Martus <erik@emartus.dev>
2021-11-10 13:58:49 -0500

    :sparkles: Add no scroll functionality while modals are open

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,32 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 import CloseIcon from '../icons/close.svg';
 
+const noScroll = () => {
+  if (document.body.classList.contains('noScroll')) {
+    const scrollY = document.body.style.top;
+    document.body.classList.remove('noScroll');
+    document.body.style.position = 'relative';
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+  } else {
+    document.body.classList.add('noScroll');
+    document.body.style.top = `-${window.scrollY}px`;
+    document.body.style.position = 'fixed';
+  }
+};
+
 function Modal({ title, id, hidden, handleHidden, children, headerChildren }) {
+  const modalId = id
+    ? `modal-${id}`
+    : `modal-${title.toLowerCase().replace(' ', '-')}`;
+  useEffect(() => {
+    noScroll();
+  }, [hidden]);
   return (
     <article
-      id={id ? `modal-${id}` : `modal-${title.toLowerCase().replace(' ', '-')}`}
+      id={modalId}
       className={`flex items-center justify-center px-2 py-8 fixed top-0 left-0 z-40 w-screen h-screen bg-gray-800 bg-opacity-75 ${
         hidden ? ' hidden' : ''
       }`}
