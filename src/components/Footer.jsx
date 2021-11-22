0aa22f8279b3e7acc4f12ac748fa7b260e24f753
Erik Martus <erik@emartus.dev>
2021-11-22 12:27:17 -0500

    :lipstick: Add underline to link on hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,18 @@ function Footer() {
   return (
     <footer className="mt-8 py-10 font-light bg-indigo-900 text-white">
       <div className="container flex justify-center">
-        <a href="https://emartus.dev" target="_blank" rel="noreferrer">
+        <p>
           Made with <IconHeart className="inline fill-current text-red-600" />{' '}
-          by Erik Martus
-        </a>
+          by{' '}
+          <a
+            href="https://emartus.dev"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:underline"
+          >
+            Erik Martus
+          </a>
+        </p>
       </div>
     </footer>
   );
